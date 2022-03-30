8a1835ded86462feda1c99b90ea83a0847d4b153
Genie-Goblin <98979003+Genie-Goblin@users.noreply.github.com>
2022-03-30 20:12:27 -0300

    Added Menu Stuff
    Menu Screen and select screen, variables to play around with for client to server stuff

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
@@ -8,8 +11,10 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Tank Game");
+		config.setTitle("Joe Mama's Tank Game");
 		config.setForegroundFPS(60);
+		config.setWindowSizeLimits(1920, 1080, 1920, 1080);
+		//config.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
