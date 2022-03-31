68ea8659977024b738c53cc7040f4fd9030a999a
FatCat1978 <Pacmandevil1@gmail.com>
2022-03-30 21:04:21 -0300

    Revert "Merge pull request #10 from Genie-Goblin/MenuStuff"
    This reverts commit 2a46b2fe3631cfc9e8acf69746830642d213b1cb, reversing
    changes made to 46438359066004bcffa7e921ba208d0246ab5902.

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,8 +1,5 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Graphics;
-import com.badlogic.gdx.Graphics.DisplayMode;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
@@ -11,10 +8,7 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setTitle("Joe Mama's Tank Game");
 		config.setForegroundFPS(60);
-		config.setWindowSizeLimits(1920, 1080, 1920, 1080);
-		//config.setFullscreenMode(Gdx.graphics.getDisplayMode());
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
